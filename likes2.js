255e60ebc4a89c4b1150516eb602e4dccccdd3e2
maliqharris <127056176+maliqharris@users.noreply.github.com>
2023-03-21 18:55:02 -0400

    Part 2

---
+++ likes2.js
@@ -0,0 +1,40 @@
+var likes = 9;
+var changeLikes = document.querySelector(".row1 h2");
+
+function likesGoUp(){
+    likes++
+    changeLikes.innerText = likes  + " like(s)"
+    console.log(likes);
+}
+var likes2 = 12;
+var changeLikes2 = document.querySelector(".row3 h2");
+
+function likesGoUp2(){
+    likes2++
+    changeLikes2.innerText = likes2 + " like(s)"
+}
+var likes3 = 9;
+var changeLikes3 = document.querySelector(".row5 h2");
+
+function likesGoUp3(){
+    likes3++
+    changeLikes3.innerText = likes3 + " like(s)"
+}
+// }
+// var likes = 12;
+// var changeLikes = document.querySelector(".row3 h2");
+
+// function likesGoUp2(){
+//     likes++
+//     changeLikes.innerText = likes  + " like(s)"
+//     console.log(likes);
+// }
+// var likes = 9;
+// var changeLikes = document.querySelector(".row5 h2");
+
+// function likesGoUp3(){
+//     likes++
+//     changeLikes.innerText = likes  + " like(s)"
+//     console.log(likes);
+// }
+
